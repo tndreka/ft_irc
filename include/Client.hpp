1ef76cec71b73230fc221dbec96e3833e4505f66
Taulant Ndreka <tndreka@3-F-12.42heilbronn.de>
2025-09-26 15:32:46 +0200

    ADD: init the headerfiles for client and server with the canonical form

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,7 +6,19 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 15:22:34 by tndreka           #+#    #+#             */
-/*   Updated: 2025/09/26 15:22:35 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/09/26 15:31:57 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+class Client
+{
+private:
+    
+public:
+    Client();
+    Client(const Client& other);
+    Client& operator=(const Client& other);
+    ~Client();
+};
--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,20 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 15:22:31 by tndreka           #+#    #+#             */
-/*   Updated: 2025/09/26 15:22:32 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/09/26 15:31:50 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
+
+class Server
+{
+private:
+    /* data */
+public:
+    Server();
+    Server(const Server& other);
+    Server& operator=(const Server& other);
+    ~Server();
+};
 
